b75dcd6b29a9979dfb19213bbd152de9517cf225
Ayushmaan Mishra <ayush99wiz@gmail.com>
2026-07-02 15:03:10 +0530

    prod bugs cleared

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -94,7 +94,7 @@ export const PipelineUI = () => {
         addNode(newNode);
       }
     },
-    [reactFlowInstance]
+    [reactFlowInstance, addNode, getNodeID]
   );
 
   const onDragOver = useCallback((event) => {
